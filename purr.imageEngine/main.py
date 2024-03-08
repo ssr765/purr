2ef913a88010eff5e3a188a90d144ec1d5240985
Sergio <64972770+ssr765@users.noreply.github.com>
2024-03-08 20:27:17 +0100

    Webp endpoint hecho

--- purr.imageEngine/main.py
+++ purr.imageEngine/main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from flask import Flask, request, jsonify
 
 from tools import webp
@@ -13,13 +15,34 @@ def root_endpoint():
 
 @app.route("/post_test", methods=["POST"])
 def post_test_endpoint():
-    return jsonify(request.json)
+    input_ = request.json.get("input")
+    output = request.json.get("output")
+    return jsonify({"input": input_, "output": output})
 
 
 @app.route("/webp", methods=["POST"])
 def webp_endpoint():
+    input_path = request.json.get("input")
+    output_path = request.json.get("output")
 
-    # webp.convert_to_webp("input.jpg", "output.webp")
+    # Comprobar que los parámetros de entrada no estén vacíos.
+    if not all([input_path, output_path]):
+        return jsonify({"error": "Input or output paths missing"}), 400
+
+    input_path = Path(input_path)
+    output_path = Path(output_path)
+
+    # Comprobar que el path de entrada exista.
+    if not input_path.exists():
+        return jsonify({"error": "Input path does not exist"}), 404
+
+    # Convertir la imagen a formato WebP.
+    success = webp.convert_to_webp(input_path, output_path)
+
+    # Comprobar si la imagen fue convertida exitosamente.
+    if not success:
+        return jsonify({"error": "Image could not be converted"}), 500
+
     return jsonify({"API": "Enpoint WIP"})
 
 
--- purr.imageEngine/tools/webp.py
+++ purr.imageEngine/tools/webp.py
@@ -1,21 +1,36 @@
+from pathlib import Path
+
 from PIL import Image
 
 
-def convert_to_webp(input_path: str, output_path: str, quality: int = 100) -> None:
+def convert_to_webp(input_path: Path, output_path: Path, quality: int = 100) -> bool:
     """
     Convierte una imagen a formato WebP.
 
     Args:
-    - input_path (str): El path de la imagen de entrada.
-    - output_path (str): El path donde se guardará la imagen convertida.
-    - quality (int, opcional): La calidad de la imagen de salida, en una escala de 0 a 100. El valor por defecto es 90.
+    - input_path: El path de la imagen de entrada.
+    - output_path: El path donde se guardará la imagen convertida.
+    - quality: La calidad de la imagen de salida, en una escala de 0 a 100. El valor por defecto es 90.
+
+    Returns:
+    - bool: Si la imagen fue convertida exitosamente.
+
+    Example:
+    >>> input_path = Path("input.jpg")
+    >>> output_path = Path("output")
+    >>> convert_to_webp(input_path, output_path)
+    True
     """
     image = Image.open(input_path)
-    image.save(output_path, "WEBP", quality=quality)
+
+    output_dir = output_path.parent
+    filename = output_path.stem
+    image.save(output_dir / (filename + ".webp"), "WEBP", quality=quality)
+
+    return True
 
 
 if __name__ == "__main__":
-    input_path = input("Imagen de entrada: ")
-    output_path = input("Imagen de salida: ")
-
+    input_path = input("Entry image: ")
+    output_path = input("Output image: ")
     convert_to_webp(input_path, output_path)
