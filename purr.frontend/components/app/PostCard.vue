a575fc6e814382e3dfe171138224f2a5de2c30b8
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-01 21:30:40 +0200

    Animación del like, se triggerea en todos los posts

--- purr.frontend/components/app/PostCard.vue
+++ purr.frontend/components/app/PostCard.vue
@@ -1,10 +1,16 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
+import gsap from 'gsap'
 
 const like = ref(false)
 
 const addLike = () => {
   like.value = !like.value
+  let tl = gsap.timeline()
+  tl.to('.post-like', { scale: 1.5, duration: 0.15 })
+  tl.to('.post-like', { scale: 0.9, duration: 0.15 })
+  tl.to('.post-like', { scale: 1.2, duration: 0.35 })
+  tl.to('.post-like', { scale: 0, duration: 0.35 })
 }
 </script>
 
@@ -13,6 +19,7 @@ const addLike = () => {
     <div class="relative">
       <img v-on:dblclick="addLike()" class="w-full rounded-lg" src="/public/img/mockcat.webp" alt="" />
       <div class="absolute p-2 bottom-0 right-0 bg-red-500 bg-opacity-50">ssr765</div>
+      <font-awesome-icon icon="fa-solid fa-heart" class="post-like scale-0 text-[100px] absolute-center text-red-500" />
     </div>
     <div class="grid col-span-2">
       <div class="p-4 pb-2 text-3xl flex gap-4">
