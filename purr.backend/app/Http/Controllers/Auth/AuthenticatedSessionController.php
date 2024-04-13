e61acd56a0c381c32e349d1916d4743df4ecb32c
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-13 15:04:21 +0200

    Devolver el usuario al registrar o login

--- purr.backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ purr.backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\V1\UserResource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -13,13 +15,13 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        return response()->json(new UserResource(Auth::user()));
     }
 
     /**
--- purr.backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ purr.backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\JsonResponse;
@@ -42,6 +43,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return response()->json($user, 201);
+        return response()->json(new UserResource($user), 201);
     }
 }
