13a37ea25593dc6298a9fed7a4a435a2b20def6b
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-15 01:16:30 +0200

    Prefijo V1 a las rutas

--- purr.backend/routes/api.php
+++ purr.backend/routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\V1\CatController;
-use App\Http\Controllers\Api\V1\PostController;
-use App\Http\Controllers\Api\V1\UserController;
-use App\Http\Resources\V1\UserResource;
+use App\Http\Controllers\Api\V1\CatController as V1CatController;
+use App\Http\Controllers\Api\V1\PostController as V1PostController;
+use App\Http\Controllers\Api\V1\UserController as V1UserController;
+use App\Http\Resources\V1\UserResource as V1UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +18,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return response()->json(new UserResource($request->user()));
-});
+Route::prefix('v1')->group(function () {
+    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+        return response()->json(new V1UserResource($request->user()));
+    });
 
-Route::apiResource('users', UserController::class)->only(['show']);
-Route::apiResource('cats', CatController::class)->only(['show', 'store']);
-Route::apiResource('posts', PostController::class)->only(['store', 'show']);
+    Route::apiResource('users', V1UserController::class)->only(['show']);
+    Route::apiResource('cats', V1CatController::class)->only(['show', 'store']);
+    Route::apiResource('posts', V1PostController::class)->only(['store', 'show']);
 
-Route::prefix('posts')->name('posts.')->group(function () {
-    Route::get('/{post}/image', [PostController::class, 'showContent'])->name('content');
+    Route::prefix('posts')->name('posts.')->group(function () {
+        Route::get('/{post}/media', [V1PostController::class, 'showContent'])->name('content');
+    });
 });
