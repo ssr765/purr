81e9f647b11efc73145aa9435e04ac9c0afcb1f3
ssr765 <sergi.serra.r@gmail.com>
2024-04-15 19:31:23 +0200

    Fix del nombre de la tabla pivote

--- purr.backend/database/migrations/2024_04_15_153328_create_cat_user_table.php
+++ purr.backend/database/migrations/2024_04_15_153328_create_cat_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_cat', function (Blueprint $table) {
+        Schema::create('cat_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('cat_id')->constrained()->onDelete('cascade');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_cat');
+        Schema::dropIfExists('cat_user');
     }
 };
