2a25270e1546663b5e7584e5739f0e1628fc5433
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-17 01:12:22 +0200

    Media input bonito

--- purr.frontend/src/components/app/create/MediaInput.vue
+++ purr.frontend/src/components/app/create/MediaInput.vue
@@ -15,19 +15,12 @@ const onChange = (e: Event) => {
 </script>
 
 <template>
-  <section>
-    <div>Añade la foto del gato</div>
-    <div class="flex items-center justify-center w-full">
-      <label for="dropzone-file" class="flex flex-col items-center justify-center w-64 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-        <div class="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-          </svg>
-          <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-          <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-        </div>
-        <input @change="onChange" id="dropzone-file" type="file" class="hidden" />
-      </label>
-    </div>
+  <section class="h-app p-16">
+    <label for="cat-content-input" class="h-full border-8 border-ctp-lavender border-dashed rounded-3xl flex flex-col items-center justify-center gap-4 lg:gap-8 p-4 hover:bg-ctp-lavender/20 cursor-pointer">
+      <span class="icon-[tabler--photo] text-[100px] lg:text-[200px]" role="img" aria-hidden="true" />
+      <div class="xl:ml-5 text-center text-5xl lg:text-[80px] lg:leading-[80px] tracking-tight font-bold">Comparte un momento felino</div>
+      <div class="text-xl lg:text-4xl text-center">{{ $t('app.create.mediaInput.content') }}</div>
+      <input @change="onChange" id="cat-content-input" type="file" class="hidden" />
+    </label>
   </section>
 </template>
