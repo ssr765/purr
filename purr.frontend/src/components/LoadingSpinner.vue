1e0543bce4e1d66a33cc2faeab54b61f77ec2744
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-17 15:00:18 +0200

    Meter el spinner en un div

--- purr.frontend/src/components/LoadingSpinner.vue
+++ purr.frontend/src/components/LoadingSpinner.vue
@@ -1,3 +1,5 @@
 <template>
-  <span class="icon-[solar--cat-linear] animate-spin text-6xl" role="img" aria-hidden="true" />
+  <div>
+    <span class="icon-[solar--cat-linear] animate-spin text-6xl" role="img" aria-hidden="true" />
+  </div>
 </template>
