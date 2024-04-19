4043d9ba4f36fdd7cfb097fec7be2d18f13d53d4
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-19 21:35:30 +0200

    Mejoras en el PurrButton

--- purr.frontend/src/components/utils/PurrButton.vue
+++ purr.frontend/src/components/utils/PurrButton.vue
@@ -3,9 +3,7 @@
 </script>
 
 <template>
-  <button type="button" class="block overflow-hidden relative focus:outline-none text-white dark:text-black bg-gradient-to-br from-ctp-lavender via-ctp-lavender to-ctp-mauve hover:bg-ctp-lavender/75 font-medium rounded-lg text-sm px-7 py-2.5 mb-2 shadow-lg shadow-ctp-lavender/35 active:translate-y-1 transition-all">
-    <span>
-      <slot />
-    </span>
+  <button type="button" class="bg-ctp-mantle block overflow-hidden relative focus:outline-none border border-ctp-lavender hover:text-white hover:dark:text-black dark:text-white text-black hover:bg-gradient-to-br from-ctp-lavender via-ctp-lavender to-ctp-mauve hover:bg-ctp-lavender/75 font-medium rounded-lg text-sm px-7 py-2.5 mb-2 shadow-lg shadow-ctp-lavender/35 active:translate-y-1 transition-all">
+    <slot />
   </button>
 </template>
