0fe265716bb6499d975866e301ffc7ba63c568ee
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-20 22:01:39 +0200

    Hacer el botón más dinámico

--- purr.frontend/src/components/utils/PurrButton.vue
+++ purr.frontend/src/components/utils/PurrButton.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-//
+defineProps({
+  noMargin: {
+    type: Boolean,
+    default: false,
+  },
+  noPaddingX: {
+    type: Boolean,
+    default: false,
+  },
+})
 </script>
 
 <template>
-  <button type="button" class="bg-ctp-mantle block overflow-hidden relative focus:outline-none border border-ctp-lavender hover:text-white hover:dark:text-black dark:text-white text-black hover:bg-gradient-to-br from-ctp-lavender via-ctp-lavender to-ctp-mauve hover:bg-ctp-lavender/75 font-medium rounded-lg text-sm px-7 py-2.5 mb-2 shadow-lg shadow-ctp-lavender/35 active:translate-y-1 transition-all">
+  <button type="button" :class="{ 'mb-2': !noMargin, 'px-7': !noPaddingX }" class="bg-ctp-mantle block overflow-hidden relative focus:outline-none border border-ctp-lavender hover:text-white hover:dark:text-black dark:text-white text-black hover:bg-gradient-to-br from-ctp-lavender via-ctp-lavender to-ctp-mauve hover:bg-ctp-lavender/75 font-medium rounded-lg text-sm py-2.5 shadow-lg shadow-ctp-lavender/35 active:translate-y-1 transition-all">
     <slot />
   </button>
 </template>
