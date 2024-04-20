bfa63cfc895cb7976ff12145afbe6654c0d07ccc
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-20 20:24:28 +0200

    Mejoras en los posts del perfil del gato

--- purr.frontend/src/components/app/cats/profile/ProfilePosts.vue
+++ purr.frontend/src/components/app/cats/profile/ProfilePosts.vue
@@ -32,9 +32,22 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-2">
-    <div v-for="post in cat.posts" :key="post.id">
-      <img :src="post.url" class="profile-post" alt="" />
+  <div class="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-2 px-4 pb-4">
+    <div v-for="post in cat.posts" :key="post.id" class="mx-auto relative">
+      <img :src="post.url" class="profile-post object-cover aspect-square w-full xl:max-w-[20dvw]" alt="" />
+      <div class="overlay bg-ctp-crust bg-opacity-50 w-full h-full absolute top-0 left-0 opacity-0 hover:opacity-100 transition-all z-50">
+        <div class="flex justify-center items-center gap-4 h-full">
+          <span class="p-2 flex flex-col justify-center gap-0.5">
+            <font-awesome-icon icon="fa-solid fa-heart" />
+            <div class="font-semibold text-center">0</div>
+          </span>
+
+          <span class="p-2 flex flex-col justify-center gap-0.5">
+            <font-awesome-icon icon="fa-solid fa-comment" />
+            <div class="font-semibold text-center">0</div>
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
