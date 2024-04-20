3bd0bb7359490d0fb794fa034b9fb6c858a12e7a
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-20 16:08:47 +0200

    409 puede tener más sentido

--- purr.backend/app/Http/Middleware/RedirectIfAuthenticated.php
+++ purr.backend/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,7 +22,7 @@ class RedirectIfAuthenticated
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
                 // return redirect(RouteServiceProvider::HOME);
-                return response()->json(['message' => 'User already logged in.'], 403);
+                return response()->json(['message' => 'User already logged in.'], 409);
             }
         }
 
