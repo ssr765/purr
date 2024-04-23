14fa1adf256ad85ac8be06b73efe01d503222374
ssr765 <sergi.serra.r@gmail.com>
2024-04-23 20:44:08 +0200

    Comentarios medio funcionales

---
+++ purr.backend/app/Http/Controllers/Api/V1/CommentLikeController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCommentLikeRequest;
+use App\Http\Requests\UpdateCommentLikeRequest;
+use App\Models\Comment;
+use App\Models\CommentLike;
+
+class CommentLikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCommentLikeRequest $request, Comment $comment)
+    {
+        $user = $request->user();
+
+        $like = CommentLike::where('user_id', $user->id)
+            ->where('comment_id', $comment->id)
+            ->first();
+
+        // Check if the user has already liked the post.
+        if ($like) {
+            return response()->json(['message' => 'Comment already liked'], 409);
+        }
+
+        // Create a new like.
+        $comment->likes()->create([
+            'user_id' => $user->id,
+        ]);
+        $comment->increment('likes_count');
+
+        return response()->json(null, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CommentLike $commentLike)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCommentLikeRequest $request, CommentLike $commentLike)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CommentLike $commentLike)
+    {
+        //
+    }
+}
--- purr.backend/routes/api.php
+++ purr.backend/routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Api\V1\CatController as V1CatController;
 use App\Http\Controllers\Api\V1\PostController as V1PostController;
 use App\Http\Controllers\Api\V1\UserController as V1UserController;
+use App\Http\Controllers\Api\V1\PostLikeController as V1PostLikeController;
+use App\Http\Controllers\Api\V1\CommentController as V1CommentController;
+use App\Http\Controllers\Api\V1\CommentLikeController as V1CommentLikeController;
 use App\Http\Resources\V1\UserResource as V1UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -42,5 +45,14 @@ Route::prefix('v1')->group(function () {
         Route::get('/{post}/media', [V1PostController::class, 'showContent'])->name('content');
         Route::post('/{post}/likes', [V1PostLikeController::class, 'store'])->name('likes.store');
         Route::delete('/{post}/likes', [V1PostLikeController::class, 'destroy'])->name('likes.destroy');
+    });
+
+
+    Route::middleware(['auth:sanctum'])->apiResource('comments', V1CommentController::class)->only(['show', 'store', 'destroy']);
+    // Comments
+    Route::middleware(['auth:sanctum'])->prefix('comments')->name('comments.')->group(function () {
+        Route::post('/{comment}', [V1CommentController::class, 'storeReply'])->name('replies.store');
+        Route::post('/{comment}/likes', [V1CommentLikeController::class, 'store'])->name('likes.store');
+        Route::delete('/{comment}/likes', [V1CommentLikeController::class, 'destroy'])->name('likes.destroy');
     });
 });
