588a37f2d77b4e92a924191a00b1f25c2bd0ba30
Sergio <64972770+ssr765@users.noreply.github.com>
2024-04-28 16:50:48 +0200

    Fuentes definitivas, Fredoka y Readex Pro

--- purr.frontend/src/components/app/cats/create/TraitsStep.vue
+++ purr.frontend/src/components/app/cats/create/TraitsStep.vue
@@ -6,8 +6,8 @@ import TraitsForm from './TraitsStep/TraitsForm.vue'
 
 <template>
   <section>
-    <h2 class="text-5xl lg:text-6xl text-center p-2 lg:p-4 tracking-tight font-medium mb-4">Cuentanos más sobre tu gato</h2>
-    <p class="text-center text-lg lg:text-xl mb-4">Ahora, cuéntanos más detalles sobre tu gato. Cuantos más detalles nos des, más destacará tu gato en purr.</p>
+    <h2 class="text-5xl lg:text-6xl text-center p-2 lg:p-4">Cuentanos más sobre tu gato</h2>
+    <p class="text-center text-lg lg:text-xl mb-8 lg:mb-16">Ahora, cuéntanos más detalles sobre tu gato. Cuantos más detalles nos des, más destacará tu gato en purr.</p>
     <TraitsForm />
   </section>
 </template>
