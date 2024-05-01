cd8c79c6b54a08a09e00b743553408e891d7f63b
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-01 19:55:04 +0200

    refactor(backend): Organización de las rutas API

--- purr.backend/routes/api.php
+++ purr.backend/routes/api.php
@@ -21,50 +21,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// V1 API
+// ----- V1 API
 Route::prefix('v1')->group(function () {
-    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-        return response()->json(new V1UserResource($request->user()->load('cats')));
-    });
-
-    Route::middleware(['auth:sanctum'])->prefix('user')->name('user.')->group(function () {
-        Route::get('/', function (Request $request) {
+    // --- Requires authentication
+    Route::middleware(['auth:sanctum'])->group(function () {
+        // Actual user data
+        Route::get('/user', function (Request $request) {
             return response()->json(new V1UserResource($request->user()->load('cats')));
-        })->name('show');
-        Route::post('/username', [V1UserController::class, 'checkUsername'])->name('checkUsername');
-        Route::post('/email', [V1UserController::class, 'checkEmail'])->name('checkEmail');
-        Route::get('/cats', [V1UserController::class, 'showCats'])->name('cats');
+        })->name('user.show');
+
+        // User utilities
+        Route::prefix('user')->name('user.')->group(function () {
+            Route::post('/username', [V1UserController::class, 'checkUsername'])->name('checkUsername');
+            Route::post('/email', [V1UserController::class, 'checkEmail'])->name('checkEmail');
+            Route::get('/cats', [V1UserController::class, 'showCats'])->name('cats');
+        });
+
+        // CRUD resources
+        Route::apiResource('users', V1UserController::class)->only(['destroy']);
+        Route::apiResource('cats', V1CatController::class)->only(['store', 'destroy']);
+        Route::apiResource('posts', V1PostController::class)->only(['store']);
+        Route::apiResource('comments', V1CommentController::class)->only(['store', 'destroy']);
+
+        // Cat routes
+        Route::post('cats/catname', [V1CatController::class, 'checkCatname'])->name('cats.checkCatname');
+        Route::get('cats/catname/{catname}', [V1CatController::class, 'showByCatname'])->name('cats.showByCatname');
+
+        // Post routes
+        Route::prefix('posts')->name('posts.')->group(function () {
+            Route::get('/{post}/media', [V1PostController::class, 'showContent'])->name('content');
+            Route::get('/{post}/media/download', [V1PostController::class, 'download'])->name('download');
+            Route::post('/{post}/likes', [V1PostLikeController::class, 'store'])->name('likes.store');
+            Route::delete('/{post}/likes', [V1PostLikeController::class, 'destroy'])->name('likes.destroy');
+        });
+
+        // Comment replies & likes routes
+        Route::prefix('comments')->name('comments.')->group(function () {
+            Route::post('/{comment}', [V1CommentController::class, 'storeReply'])->name('replies.store');
+            Route::post('/{comment}/likes', [V1CommentLikeController::class, 'store'])->name('likes.store');
+            Route::delete('/{comment}/likes', [V1CommentLikeController::class, 'destroy'])->name('likes.destroy');
+        });
     });
 
+    // --- No authentication required
+    // Show resources
     Route::apiResource('users', V1UserController::class)->only(['show']);
-    Route::middleware(['auth:sanctum'])->apiResource('users', V1UserController::class)->only(['destroy']);
+    Route::apiResource('cats', V1CatController::class)->only(['show']);
+    Route::apiResource('posts', V1PostController::class)->only(['index', 'show']);
+    Route::apiResource('comments', V1CommentController::class)->only(['show']);
 
+    // Cat routes
     Route::prefix('cats')->name('cats.')->group(function () {
         Route::get('/random', [V1CatController::class, 'random'])->name('random');
         Route::get('/{cat}/avatar', [V1CatController::class, 'avatar'])->name('avatar');
-    });
-    Route::apiResource('cats', V1CatController::class)->only(['show']);
-    Route::middleware(['auth:sanctum'])->apiResource('cats', V1CatController::class)->only(['store', 'destroy']);
-    Route::post('cats/catname', [V1CatController::class, 'checkCatname'])->name('cats.checkCatname');
-    Route::get('cats/catname/{catname}', [V1CatController::class, 'showByCatname'])->name('cats.showByCatname');
-
-    // Posts
-    Route::apiResource('posts', V1PostController::class)->only(['index', 'show']);
-    Route::middleware(['auth:sanctum'])->apiResource('posts', V1PostController::class)->only(['store']);
-
-    Route::prefix('posts')->name('posts.')->group(function () {
-        Route::get('/{post}/media', [V1PostController::class, 'showContent'])->name('content');
-        Route::get('/{post}/media/download', [V1PostController::class, 'download'])->name('download');
-        Route::middleware(['auth:sanctum'])->post('/{post}/likes', [V1PostLikeController::class, 'store'])->name('likes.store');
-        Route::middleware(['auth:sanctum'])->delete('/{post}/likes', [V1PostLikeController::class, 'destroy'])->name('likes.destroy');
-    });
-
-
-    Route::middleware(['auth:sanctum'])->apiResource('comments', V1CommentController::class)->only(['show', 'store', 'destroy']);
-    // Comments
-    Route::middleware(['auth:sanctum'])->prefix('comments')->name('comments.')->group(function () {
-        Route::post('/{comment}', [V1CommentController::class, 'storeReply'])->name('replies.store');
-        Route::post('/{comment}/likes', [V1CommentLikeController::class, 'store'])->name('likes.store');
-        Route::delete('/{comment}/likes', [V1CommentLikeController::class, 'destroy'])->name('likes.destroy');
     });
 });
