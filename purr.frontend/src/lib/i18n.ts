3a978f13d5664c2ecfc647bb5a4835f36ed7ce46
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-05 21:03:08 +0200

    feat(frontend): Internacionalización completa de crear publicaciones

--- purr.frontend/src/lib/i18n.ts
+++ purr.frontend/src/lib/i18n.ts
@@ -1,6 +1,8 @@
 import { createI18n } from 'vue-i18n'
 import ES from '@/locales/es.json'
 import EN from '@/locales/en.json'
+import CA from '@/locales/ca.json'
+import JA from '@/locales/ja.json'
 
 export const i18n = createI18n({
   legacy: false,
@@ -9,5 +11,7 @@ export const i18n = createI18n({
   messages: {
     en: EN,
     es: ES,
+    ca: CA,
+    ja: JA,
   },
 })
