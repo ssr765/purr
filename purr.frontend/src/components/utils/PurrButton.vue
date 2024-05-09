76a7bd63fc0054ec2ce5dfa5a355ee90f5e9b3b1
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-09 20:11:39 +0200

    feat(frontend): Props para quitar el margin/padding

--- purr.frontend/src/components/utils/PurrButton.vue
+++ purr.frontend/src/components/utils/PurrButton.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 
 <template>
-  <button type="button" :class="{ 'mb-2': !noMargin, 'px-7': !noPaddingX }" class="bg-ctp-mantle block overflow-hidden relative focus:outline-none border border-ctp-lavender hover:text-white hover:dark:text-black dark:text-white text-black hover:bg-gradient-to-br from-ctp-lavender via-ctp-lavender to-ctp-mauve hover:bg-ctp-lavender/75 font-medium rounded-lg text-sm py-2.5 shadow-lg shadow-ctp-lavender/35 active:translate-y-1 disabled:active:translate-y-0 disabled:hover:from-red-500/50 disabled:hover:to-red-500/50 transition-all">
+  <button type="button" :class="{ 'mb-2': !noMargin, 'px-7': !noPaddingX }" class="bg-ctp-mantle block overflow-hidden relative focus:outline-none border border-ctp-lavender hover:text-white hover:dark:text-black dark:text-white text-black hover:bg-gradient-to-br from-ctp-lavender via-ctp-lavender to-ctp-mauve hover:bg-ctp-lavender/75 font-medium rounded-lg text-sm py-2.5 shadow-lg shadow-ctp-lavender/35 active:translate-y-1 disabled:active:translate-y-0 disabled:hover:from-red-500/50ç disabled:hover:to-red-500/50ç transition-all">
     <slot />
   </button>
 </template>
