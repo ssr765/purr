9ba28e551dc6cea9500c92fb28e9817749363730
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-09 02:18:01 +0200

    feat(frontend)!: Quitar margin bottom al botón

--- purr.frontend/src/components/utils/RegularButton.vue
+++ purr.frontend/src/components/utils/RegularButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="border border-ctp-lavender text-white mb-2 text-sm bg-ctp-lavender/20 hover:bg-ctp-lavender hover:text-black py-2.5 px-7 rounded-lg transition-all">
+  <button class="border border-ctp-lavender text-white text-sm bg-ctp-lavender/20 hover:bg-ctp-lavender hover:text-black py-2.5 px-7 rounded-lg transition-all">
     <slot />
   </button>
 </template>
