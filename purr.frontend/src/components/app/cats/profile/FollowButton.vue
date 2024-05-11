8f3b01d6879ef5d933895c87fcb76328538b33ba
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-11 18:35:26 +0200

    fix(frontend): Toast si no está autenticado

--- purr.frontend/src/components/app/cats/profile/FollowButton.vue
+++ purr.frontend/src/components/app/cats/profile/FollowButton.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import LoadingSpinner from '@/components/utils/LoadingSpinner.vue'
 import RegularButton from '@/components/utils/RegularButton.vue'
 import type { Cat } from '@/models/Cat'
+import { useAuthStore } from '@/stores/authStore'
 import { useCatStore } from '@/stores/catStore'
+import { storeToRefs } from 'pinia'
 import type { PropType } from 'vue'
+import { toast } from 'vue-sonner'
 
 const catStore = useCatStore()
+const { user } = storeToRefs(useAuthStore())
 
 defineProps({
   cat: {
@@ -23,10 +26,14 @@ const unfollow = () => {
   console.log('unfollow')
   catStore.unfollow()
 }
+
+const error = () => {
+  toast.warning('Debes iniciar sesión para seguir a un gato')
+}
 </script>
 
 <template>
-  <RegularButton @click="cat.followed ? unfollow() : follow()" noPaddingX noPaddingY class="py-2 px-4">
+  <RegularButton @click="user ? (cat.followed ? unfollow() : follow()) : error()" noPaddingX noPaddingY class="py-2 px-4">
     <span v-if="!cat.followed">Seguir</span>
     <span v-else>Dejar de seguir</span>
   </RegularButton>
