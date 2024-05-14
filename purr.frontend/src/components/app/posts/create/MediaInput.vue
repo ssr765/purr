b5cf2342a66ae685cc4685883d27870ac0687687
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-14 23:44:10 +0200

    feat(frontend): Validaciones para las imágenes subidas

--- purr.frontend/src/components/app/posts/create/MediaInput.vue
+++ purr.frontend/src/components/app/posts/create/MediaInput.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import type { PostInput } from '@/models/Post'
 import { useCreatePostStore } from '@/stores/createPostStore'
+import { toast } from 'vue-sonner'
 
 const createPostStore = useCreatePostStore()
 
@@ -8,6 +8,18 @@ const onChange = (e: Event) => {
   const target = e.target as HTMLInputElement
   const file = target.files?.[0]
   if (!file) return
+
+  // Check the file size.
+  if (file.size > 1024 * 1024 * 8) {
+    toast.error('La imagen no puede ser mayor a 8MB')
+    return
+  }
+
+  // Check the file type.
+  if (!['image/webp', 'image/jpeg', 'image/png'].includes(file.type)) {
+    toast.error('Formato de imagen no soportado')
+    return
+  }
 
   // Start creating the post.
   createPostStore.onImageUpload(file)
@@ -20,6 +32,7 @@ const onChange = (e: Event) => {
       <span class="icon-[tabler--photo] text-[100px] lg:text-[200px]" role="img" aria-hidden="true" />
       <div class="xl:ml-5 text-center text-4xl lg:text-6xl tracking-tight font-semibold leading-none font-heading">{{ $t('app.posts.create.mediaInput.title') }}</div>
       <div class="text-xl lg:text-3xl text-center font-heading">{{ $t('app.posts.create.mediaInput.content') }}</div>
+      <div class="text-center font-heading">Máx. 8MB, formatos: .webp, .jpg, .png.</div>
       <input @change="onChange" id="cat-content-input" type="file" class="hidden" />
     </label>
   </section>
