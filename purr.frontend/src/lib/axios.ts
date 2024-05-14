c70ee2b5ee12862f6e57375f6e33f19d12f28dc2
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-14 12:16:10 +0200

    feat(frontend): Interceptor para 429 (no funciona)

--- purr.frontend/src/lib/axios.ts
+++ purr.frontend/src/lib/axios.ts
@@ -1,5 +1,6 @@
 import Axios from 'axios'
-
+import { toast } from 'vue-sonner'
+import { useI18n } from 'vue-i18n'
 export interface errorData {
   message: string
   errors?: Record<string, string[]>
@@ -13,5 +14,22 @@ const axios = Axios.create({
   withCredentials: true,
   withXSRFToken: true,
 })
+
+// Too Many Requests interceptor
+axios.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    const { t } = useI18n()
+    console.log(error)
+    if (error.response && error.response.status === 429) {
+      console.log('Too Many Requests')
+      toast.error(t('errors.tooManyRequests'))
+    }
+
+    return Promise.reject(error)
+  },
+)
 
 export default axios
