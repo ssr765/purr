8dd5334746753ed3e6e642bf5cd89b0fe5b1dd90
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-15 18:59:59 +0200

    feat(toastification): Add a cat

--- purr.frontend/src/components/app/cats/create/AddACat/AddCatForm.vue
+++ purr.frontend/src/components/app/cats/create/AddACat/AddCatForm.vue
@@ -12,11 +12,14 @@ import type { AxiosError } from 'axios'
 import { toast } from 'vue-sonner'
 import { storeToRefs } from 'pinia'
 import { useAuthStore } from '@/stores/authStore'
+import { useI18n } from 'vue-i18n'
 
 const catService = useCatService()
 const router = useRouter()
 const authStore = useAuthStore()
 const { user } = storeToRefs(authStore)
+
+const { t } = useI18n()
 
 const loading = ref(false)
 
@@ -24,13 +27,13 @@ const formSchema = yup.object({
   catname: yup
     .string()
     .transform((curr, orig) => (orig === '' ? undefined : curr))
-    .required('El catname es obligatorio')
-    .matches(/^[\w\d\.]{3,30}$/, 'El catname no es válido'),
+    .required(t('app.cats.create.addACat.validators.catname.required'))
+    .matches(/^[\w\d\.]{3,30}$/, t('app.cats.create.addACat.validators.catname.regex')),
 
   password: yup
     .string()
     .transform((curr, orig) => (orig === '' ? undefined : curr))
-    .required('La contraseña es obligatoria'),
+    .required(t('app.cats.create.addACat.validators.password.required')),
 })
 
 const { handleSubmit, values } = useForm({
@@ -46,14 +49,16 @@ const submit = handleSubmit(async () => {
     }
 
     user.value!.cats.push(cat)
-    router.push({ name: 'app-cats-profile', params: { catname: values.catname } })
+    router.push({ name: 'app-cats-profile', params: { catname: cat.catname } })
+    toast.success(t('app.cats.create.addACat.toast.success', { cat_name: cat.name }))
   } catch (error) {
     const axiosError = error as AxiosError
     if (axiosError.response?.status === 409) {
-      toast.error('Ya tienes el gato con ese catname')
-    }
-    if (axiosError.response?.status === 403) {
-      toast.error('Contraseña incorrecta')
+      toast.error('El gato con ese catname ya te pertenece')
+    } else if (axiosError.response?.status === 403) {
+      toast.error(t('app.cats.create.addACat.toast.wrongPassword'))
+    } else {
+      toast.error(t('app.cats.create.addACat.toast.error'))
     }
   } finally {
     loading.value = false
@@ -65,7 +70,7 @@ const submit = handleSubmit(async () => {
   <div class="space-y-6 max-w-screen-md mx-auto">
     <FormField v-slot="{ componentField }" name="catname">
       <FormItem>
-        <FormLabel class="font-bold block mb-2 text-sm text-ctp-text">Catname del gato</FormLabel>
+        <FormLabel class="font-bold block mb-2 text-sm text-ctp-text">{{ $t('app.cats.create.addACat.content.catname') }}</FormLabel>
         <FormControl>
           <Input class="block bg-ctp-mantle border border-ctp-lavender text-ctp-text text-sm rounded-lg focus:ring-ctp-lavender focus:border-ctp-lavender w-full p-2.5" type="text" v-bind="componentField" />
         </FormControl>
@@ -75,14 +80,14 @@ const submit = handleSubmit(async () => {
 
     <FormField v-slot="{ componentField }" name="password">
       <FormItem>
-        <FormLabel class="font-bold block mb-2 text-sm text-ctp-text">Contraseña</FormLabel>
+        <FormLabel class="font-bold block mb-2 text-sm text-ctp-text">{{ $t('app.cats.create.addACat.content.password') }}</FormLabel>
         <FormControl>
           <Input class="block bg-ctp-mantle border border-ctp-lavender text-ctp-text text-sm rounded-lg focus:ring-ctp-lavender focus:border-ctp-lavender w-full p-2.5" type="password" v-bind="componentField" />
         </FormControl>
         <FormMessage />
       </FormItem>
     </FormField>
-    <PurrButton v-if="!loading" class="w-full lg:w-auto mx-auto" @click="submit">Añadir gato</PurrButton>
+    <PurrButton v-if="!loading" class="w-full lg:w-auto mx-auto" @click="submit">{{ $t('app.cats.create.addACat.content.button') }}</PurrButton>
     <LoadingSpinner v-else class="text-4xl mx-auto" />
   </div>
 </template>
