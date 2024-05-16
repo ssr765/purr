580cf878d1531901b4558f0c44c37f92cf634a4f
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-16 10:57:56 +0200

    feat(toastification): Layout y utilidad

--- purr.frontend/src/composables/imageChecker.ts
+++ purr.frontend/src/composables/imageChecker.ts
@@ -1,16 +1,22 @@
+import { useI18n } from 'vue-i18n'
 import { toast } from 'vue-sonner'
 
 export function useImageChecker() {
+  const { t } = useI18n()
   const checkFile = (file: File) => {
     // Check the file size.
     if (file.size > 1024 * 1024 * 8) {
-      toast.error('La imagen no puede ser mayor a 8MB')
+      toast.error(t('app.utils.imageChecker.fileSizeError', { mb: 8 }))
       return false
     }
 
     // Check the file type.
     if (!['image/webp', 'image/jpeg', 'image/png'].includes(file.type)) {
-      toast.error('Formato de imagen no soportado')
+      toast.error(
+        t('app.utils.imageChecker.fileTypeError', {
+          formats: 'webp, jpeg, png',
+        }),
+      )
       return false
     }
 
