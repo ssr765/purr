83cc2e37ed7ff3de31a04acf1ac521f6f469f057
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-19 20:55:51 +0200

    feat(frontend): Scroll responsive y tip para el username

--- purr.frontend/src/components/auth/LoginSide.vue
+++ purr.frontend/src/components/auth/LoginSide.vue
@@ -17,30 +17,32 @@ const password = ref('')
 </script>
 
 <template>
-  <aside class="h-dvh flex flex-col items-center justify-center w-full gap-16 p-16">
-    <PurrLogo class="size-36" />
+  <aside class="h-dvh overflow-auto">
+    <div class="flex flex-col items-center justify-center w-full gap-16 p-16">
+      <PurrLogo class="size-36" />
 
-    <form @submit.prevent="login()" class="max-w-sm w-full mx-auto space-y-8">
-      <div class="space-y-4">
-        <div class="relative z-0 w-full group">
-          <input v-model="email" type="email" id="login_email" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
-          <label for="login_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.login.email') }}</label>
+      <form @submit.prevent="login()" class="max-w-sm w-full mx-auto space-y-8">
+        <div class="space-y-4">
+          <div class="relative z-0 w-full group">
+            <input v-model="email" type="email" id="login_email" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
+            <label for="login_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.login.email') }}</label>
+          </div>
+          <div class="relative z-0 w-full group">
+            <input v-model="password" type="password" id="login_password" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
+            <label for="login_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.login.password') }}</label>
+          </div>
         </div>
-        <div class="relative z-0 w-full group">
-          <input v-model="password" type="password" id="login_password" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
-          <label for="login_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.login.password') }}</label>
+        <div v-if="!authStore.loading" class="flex flex-col gap-2">
+          <PurrButton type="submit" class="mx-auto w-full"> {{ $t('auth.login.button') }}</PurrButton>
+          <GoogleButton />
         </div>
-      </div>
-      <div v-if="!authStore.loading" class="flex flex-col gap-2">
-        <PurrButton type="submit" class="mx-auto w-full"> {{ $t('auth.login.button') }}</PurrButton>
-        <GoogleButton />
-      </div>
-      <div v-else>
-        <LoadingSpinner class="text-5xl" />
-      </div>
-    </form>
-    <p class="text-center">
-      {{ $t('auth.login.noAccount.content') }} <RouterLink class="text-ctp-lavender" :to="{ name: 'auth-register' }"> {{ $t('auth.login.noAccount.link') }} </RouterLink>
-    </p>
+        <div v-else>
+          <LoadingSpinner class="text-5xl" />
+        </div>
+      </form>
+      <p class="text-center">
+        {{ $t('auth.login.noAccount.content') }} <RouterLink class="text-ctp-lavender" :to="{ name: 'auth-register' }"> {{ $t('auth.login.noAccount.link') }} </RouterLink>
+      </p>
+    </div>
   </aside>
 </template>
--- purr.frontend/src/components/auth/RegisterSide.vue
+++ purr.frontend/src/components/auth/RegisterSide.vue
@@ -27,44 +27,47 @@ const password_confirmation = ref('')
 </script>
 
 <template>
-  <aside class="h-dvh flex flex-col items-center justify-center w-full gap-16 p-16">
-    <PurrLogo class="size-36" />
+  <aside class="h-dvh overflow-auto">
+    <div class="flex flex-col items-center justify-center w-full gap-16 p-16">
+      <PurrLogo class="size-36" />
 
-    <form @submit.prevent="register()" class="max-w-sm w-full mx-auto space-y-8">
-      <div class="space-y-4">
-        <div class="relative z-0 w-full group">
-          <input v-model="name" type="text" id="register_name" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
-          <label for="register_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.name') }} </label>
+      <form @submit.prevent="register()" class="max-w-sm w-full mx-auto space-y-8">
+        <div class="space-y-4">
+          <div class="relative z-0 w-full group">
+            <input v-model="name" type="text" id="register_name" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
+            <label for="register_name" class="peer-focus:font-medium absolute text-sm text-ctp-text duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.name') }} </label>
+          </div>
+          <div class="relative z-0 w-full group">
+            <input v-model="username" type="text" id="register_username" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
+            <label for="register_username" class="peer-focus:font-medium absolute text-sm text-ctp-text duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.username') }} </label>
+            <p class="text-xs text-ctp-text mt-2">{{ $t('auth.register.usernameHelp') }}</p>
+          </div>
+          <div class="relative z-0 w-full group">
+            <input v-model="email" type="email" id="register_email" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
+            <label for="register_email" class="peer-focus:font-medium absolute text-sm text-ctp-text duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.email') }} </label>
+          </div>
+          <div class="relative z-0 w-full group">
+            <input v-model="password" type="password" id="register_password" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
+            <label for="register_password" class="peer-focus:font-medium absolute text-sm text-ctp-text duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.password') }} </label>
+          </div>
+          <div class="relative z-0 w-full group">
+            <input v-model="password_confirmation" type="password" id="register_repeat_password" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
+            <label for="register_repeat_password" class="peer-focus:font-medium absolute text-sm text-ctp-text duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.passwordConfirmation') }} </label>
+          </div>
         </div>
-        <div class="relative z-0 w-full group">
-          <input v-model="username" type="text" id="register_username" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
-          <label for="register_username" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.username') }} </label>
-        </div>
-        <div class="relative z-0 w-full group">
-          <input v-model="email" type="email" id="register_email" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
-          <label for="register_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.email') }} </label>
-        </div>
-        <div class="relative z-0 w-full group">
-          <input v-model="password" type="password" id="register_password" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
-          <label for="register_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.password') }} </label>
-        </div>
-        <div class="relative z-0 w-full group">
-          <input v-model="password_confirmation" type="password" id="register_repeat_password" class="block py-2.5 px-0 w-full text-sm text-ctp-text bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-ctp-lavender peer" placeholder=" " required />
-          <label for="register_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-ctp-lavender peer-focus:dark:text-ctp-lavender peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ $t('auth.register.passwordConfirmation') }} </label>
-        </div>
-      </div>
 
-      <div v-if="!authStore.loading" class="flex flex-col gap-2">
-        <PurrButton type="submit" class="mx-auto w-full"> {{ $t('auth.register.button') }}</PurrButton>
-        <GoogleButton />
-      </div>
-      <div v-else>
-        <LoadingSpinner class="text-5xl" />
-      </div>
-    </form>
+        <div v-if="!authStore.loading" class="flex flex-col gap-2">
+          <PurrButton type="submit" class="mx-auto w-full"> {{ $t('auth.register.button') }}</PurrButton>
+          <GoogleButton />
+        </div>
+        <div v-else>
+          <LoadingSpinner class="text-5xl" />
+        </div>
+      </form>
 
-    <p class="text-center">
-      {{ $t('auth.register.haveAccount.content') }} <RouterLink class="text-ctp-lavender" :to="{ name: 'auth-login' }"> {{ $t('auth.register.haveAccount.link') }} </RouterLink>
-    </p>
+      <p class="text-center">
+        {{ $t('auth.register.haveAccount.content') }} <RouterLink class="text-ctp-lavender" :to="{ name: 'auth-login' }"> {{ $t('auth.register.haveAccount.link') }} </RouterLink>
+      </p>
+    </div>
   </aside>
 </template>
