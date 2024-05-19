a0105231e924eb00e4679c5e8aa5020e3e50c16d
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-19 20:56:23 +0200

    feat(frontend): No se intenta buscar la traducción del error

--- purr.frontend/src/composables/responseToaster.ts
+++ purr.frontend/src/composables/responseToaster.ts
@@ -1,16 +1,14 @@
 import { toast } from 'vue-sonner'
 import type { errorData } from '@/lib/axios'
 import type { AxiosError } from 'axios'
-import { useI18n } from 'vue-i18n'
 
 export function useResponseToaster() {
   const removeDots = (text: string) => text.replace(/\./g, '')
-  const { t } = useI18n()
 
   const toastResponse = (error: AxiosError) => {
     if (error.response) {
       const data = error.response?.data as errorData
-      toast(t(`response.${removeDots(data.message)}`))
+      toast(removeDots(data.message))
     }
   }
 
