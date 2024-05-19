98c01421e77d319e3c45aded6b0b388f8469c944
Sergio <64972770+ssr765@users.noreply.github.com>
2024-05-19 22:37:40 +0200

    fix(frontend): Quitar IDs duplicados

--- purr.frontend/src/components/app/cats/create/PasswordStep/PasswordForm.vue
+++ purr.frontend/src/components/app/cats/create/PasswordStep/PasswordForm.vue
@@ -7,12 +7,12 @@ const createCatStore = useCreateCatStore()
 <template>
   <div class="space-y-6 max-w-screen-md mx-auto">
     <div>
-      <label for="first_name" class="block mb-2 text-sm font-medium text-ctp-text">{{ $t('app.cats.create.createCat.steps.password.form.password') }} *</label>
-      <input v-model="createCatStore.password" type="password" id="first_name" class="block bg-ctp-mantle border border-ctp-lavender text-ctp-text text-sm rounded-lg focus:ring-ctp-lavender focus:border-ctp-lavender w-full p-2.5" required />
+      <label for="password_step_password" class="block mb-2 text-sm font-medium text-ctp-text">{{ $t('app.cats.create.createCat.steps.password.form.password') }} *</label>
+      <input v-model="createCatStore.password" type="password" id="password_step_password" class="block bg-ctp-mantle border border-ctp-lavender text-ctp-text text-sm rounded-lg focus:ring-ctp-lavender focus:border-ctp-lavender w-full p-2.5" required />
     </div>
     <div>
-      <label for="first_name" class="block mb-2 text-sm font-medium text-ctp-text">{{ $t('app.cats.create.createCat.steps.password.form.passwordConfirmation') }} *</label>
-      <input v-model="createCatStore.confirmPassword" type="password" id="first_name" class="block bg-ctp-mantle border border-ctp-lavender text-ctp-text text-sm rounded-lg focus:ring-ctp-lavender focus:border-ctp-lavender w-full p-2.5" required />
+      <label for="password_step_password_confirmation" class="block mb-2 text-sm font-medium text-ctp-text">{{ $t('app.cats.create.createCat.steps.password.form.passwordConfirmation') }} *</label>
+      <input v-model="createCatStore.confirmPassword" type="password" id="password_step_password_confirmation" class="block bg-ctp-mantle border border-ctp-lavender text-ctp-text text-sm rounded-lg focus:ring-ctp-lavender focus:border-ctp-lavender w-full p-2.5" required />
     </div>
   </div>
 </template>
